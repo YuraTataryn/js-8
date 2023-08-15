f093f55d485b536766e39bb31406407dff898a14
YuraTataryn <127976814+YuraTataryn@users.noreply.github.com>
2023-08-15 09:59:53 -0700

    add

--- dist/01-gallery.53390864.js
+++ dist/01-gallery.53390864.js
@@ -503,12 +503,49 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"6XVkV":[function(require,module,exports) {
-// Add imports above this line
-var _galleryItems = require("./gallery-items");
-// Change code below this line
-console.log((0, _galleryItems.galleryItems));
+var _galleryItemsJs = require("./gallery-items.js");
+const galleryListItem = document.querySelector(".gallery");
+function createMarkup(arr) {
+    return arr.map(({ preview , original , description  })=>`<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      width="250px"
+    />
+  </a>
+</li>`).join("");
+}
+galleryListItem.insertAdjacentHTML("beforeend", createMarkup((0, _galleryItemsJs.galleryItems)));
+const galleryLinks = document.querySelectorAll(".gallery__link");
+galleryLinks.forEach((galleryLink)=>{
+    galleryLink.addEventListener("click", function(clickEvent) {
+        clickEvent.preventDefault();
+        if (clickEvent.target.tagName !== "IMG") return;
+        const bigImage = {
+            original: clickEvent.target.dataset.source,
+            description: clickEvent.target.alt
+        };
+        const modalWindow = basicLightbox.create(`<div class="modal">
+          <img src="${bigImage.original}" alt="${bigImage.description}" width="800px">
+        </div>`, {
+            onClose: (instance)=>{
+                instance.element().remove();
+            },
+            onShow: (instance)=>{
+                const escapeWindow = (escEvent)=>{
+                    if (escEvent.key === "Escape") instance.close();
+                };
+                document.addEventListener("keydown", escapeWindow);
+            }
+        });
+        modalWindow.show();
+    });
+});
 
-},{"./gallery-items":"9C7dK"}],"9C7dK":[function(require,module,exports) {
+},{"./gallery-items.js":"9C7dK"}],"9C7dK":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "galleryItems", ()=>galleryItems);
--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,57 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const galleryListItem = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      width="250px"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+galleryListItem.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+const galleryLinks = document.querySelectorAll(".gallery__link");
+
+galleryLinks.forEach((galleryLink) => {
+  galleryLink.addEventListener("click", function (clickEvent) {
+    clickEvent.preventDefault();
+    if (clickEvent.target.tagName !== 'IMG') {
+      return;
+    }
+
+    const bigImage = {
+      original: clickEvent.target.dataset.source,
+      description: clickEvent.target.alt
+    };
+    const modalWindow = basicLightbox.create(
+        `<div class="modal">
+          <img src="${bigImage.original}" alt="${bigImage.description}" width="800px">
+        </div>`,
+        {
+          onClose: (instance) => {
+            instance.element().remove();
+          },
+          onShow: (instance) => {
+            const escapeWindow = (escEvent) => {
+              if (escEvent.key === "Escape") {
+                instance.close();
+              }
+            };
+            document.addEventListener("keydown", escapeWindow);
+          }
+        }
+    );
+      modalWindow.show();
+    });
+});
